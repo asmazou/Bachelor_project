ff3f59e1c89548d16b610cebb00211bf5ba7d201
asmazou <89570271+asmazou@users.noreply.github.com>
2024-03-25 14:28:25 +0100

    oppdatering

---
+++ source/javascripts/percivable.js
@@ -0,0 +1,24 @@
+const button = document.querySelector(".btn"),
+      elements = document.querySelectorAll(".element");
+
+button.addEventListener("click", ()=> {
+      button.classList.toggle("open");
+});
+
+elements.forEach(element => {
+      element.addEventListener("click", () => {
+            element.classList.toggle("checked");
+
+            let checked = document.querySelectorAll(".checked");
+            let buttonText = document.querySelector(".text-btn");
+
+            if(checked && checked.length > 0){
+                  buttonText.innerText = '${checked.length} Checked';
+            }else{
+                  buttonText.innerText = "1.1 Text Alternative"
+            }
+      });
+});
+
+
+
