73aad8137aedbeaf4783d779a281bc5b69613e3e
asmazou <89570271+asmazou@users.noreply.github.com>
2024-05-09 13:13:52 +0200

    satt inn data-component i html og js

--- source/javascripts/footer.js
+++ source/javascripts/footer.js
@@ -1,61 +1,67 @@
-const checklistContainers = document.querySelectorAll(".checklist-container");
+document.querySelectorAll('[data-component="checklist"]').forEach((component) => {
+    const checklistContainers = component.querySelectorAll(".checklist-container");
 
-checklistContainers.forEach((checklistContainer) => {
-    const button = checklistContainer.querySelector(".btn");
-    const elements = checklistContainer.querySelectorAll(".element");
-    const title = button.innerText;
 
-    button.addEventListener("click", ()=> {
-        button.classList.toggle("open");
+    checklistContainers.forEach((checklistContainer) => {
+        const button = checklistContainer.querySelector(".btn");
+        const elements = checklistContainer.querySelectorAll(".element");
+        const title = button.innerText;
+
+        button.addEventListener("click", ()=> {
+            button.classList.toggle("open");
+        });
+
+        elements.forEach((element) => {
+            element.addEventListener('click', () => {
+                element.classList.toggle("checked");
+
+                const checked = checklistContainer.querySelectorAll(".element.checked").length;
+                const total = elements.length;
+                const buttonText = button.querySelector(".text-btn");
+
+                if (checked === 0) {
+                    buttonText.innerText = title;
+                }
+                else {
+                    buttonText.innerText = `${title}: ${checked} / ${total} Checked`;
+                }
+            });
+        })
     });
 
-    elements.forEach((element) => {
-        element.addEventListener('click', () => {
-            element.classList.toggle("checked");
 
-            const checked = checklistContainer.querySelectorAll(".element.checked").length;
-            const total = elements.length;
-            const buttonText = button.querySelector(".text-btn");
+    const slides = component.querySelector('.slides');
 
-            if (checked === 0) {
-                buttonText.innerText = title;
-            }
-            else {
-                buttonText.innerText = `${title}: ${checked} / ${total} Checked`;
-            }
-        });
-    })
+    const arrowRight = component.querySelector('.right');
+    const arrowLeft = component.querySelector('.left');
+
+    var indexSection = 0;
+
+    arrowRight.addEventListener('click', function () {
+        setSlide(1);
+    });
+
+    arrowLeft.addEventListener('click', function () {
+        setSlide(-1);
+    });
+
+
+
+
+
+    const setSlide = (delta) => {
+        let nextIndex = indexSection + delta;
+        const maxIndex = slides.children.length - 1;
+        if (nextIndex < 0) {
+            nextIndex = maxIndex;
+        }
+        else if (nextIndex > maxIndex) {
+            nextIndex = 0;
+        }
+
+        console.log(nextIndex, maxIndex);
+        indexSection = nextIndex;
+        slides.style = `--slide-index: ${indexSection}`;
+    };
+
 });
-
-
-const slides = document.querySelector('.slides');
-
-const arrowRight = document.querySelector('.right');
-const arrowLeft = document.querySelector('.left');
-
-var indexSection = 0;
-
-arrowRight.addEventListener('click', function () {
-    setSlide(1);
-});
-
-arrowLeft.addEventListener('click', function () {
-    setSlide(-1);
-});
-
-
-
-const setSlide = (delta) => {
-    let nextIndex = indexSection + delta;
-    const maxIndex = slides.children.length - 1;
-    if (nextIndex < 0) {
-        nextIndex = maxIndex;
-    }
-    else if (nextIndex > maxIndex) {
-        nextIndex = 0;
-    }
-
-    console.log(nextIndex, maxIndex);
-    indexSection = nextIndex;
-    slides.style = `--slide-index: ${indexSection}`;
-};
